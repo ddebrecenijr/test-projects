01f034ff4ff6d0587d9c8e4871a5101d855abd2e
David Debreceni Jr <davidjr@debreceni.net>
2020-07-30 13:55:50 -0500

    Get zipkin working

---
+++ zipkin/README.md
@@ -0,0 +1,12 @@
+# Exploring OpenZipkin
+Run zipkin server
+
+`docker run -d -p 9411:9411 openzipkin/zipkin`
+
+Run Web API
+
+`dotnet run`
+
+Make request
+
+`https://localhost:5001/weatherforecast`
--- zipkin/Startup.cs
+++ zipkin/Startup.cs
@@ -10,6 +10,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using zipkin4net;
+using zipkin4net.Tracers.Zipkin;
+using zipkin4net.Transport;
+using zipkin4net.Transport.Http;
+using zipkin4net.Middleware;
+
 
 namespace zipkin
 {
@@ -29,13 +35,27 @@ namespace zipkin
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            var appLifetime = app.ApplicationServices.GetService<IHostApplicationLifetime>();
+            appLifetime.ApplicationStarted.Register(() => {
+                TraceManager.SamplingRate = 1.0f;
+                
+                var httpSender = new HttpZipkinSender("http://localhost:9411", "application/json");
+                var tracer = new ZipkinTracer(httpSender, new JSONSpanSerializer());
+
+                TraceManager.RegisterTracer(tracer);
+                TraceManager.Start(new TracingLogger(loggerFactory, nameof(Startup)));
+            });
+
+            appLifetime.ApplicationStopped.Register(() => TraceManager.Stop());
+
+            app.UseTracing("test-web-app");
             app.UseHttpsRedirection();
 
             app.UseRouting();
